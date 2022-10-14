2f83be693165ebfb4b2de1c48e5c4780f5811742
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-10-14 16:22:16 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      Hello Oleksandr
-    </div>
-  );
-};
+import React, { Component } from 'react'
+import { nanoid } from 'nanoid';
+
+class App extends Component {
+  state = {
+    name: "",
+    email: "",
+    password: "",
+  };
+
+  idElenent = nanoid();
+
+  // Відповідає за оновлення стану
+  handleChange = evt => {
+    const { name, value } = evt.target;
+    this.setState({ [name]: value });
+  };
+
+  // Викликається під час відправлення форми
+  handleSubmit = evt => {
+    evt.preventDefault();
+    console.log(`Signed up as: ${this.state.name}`);
+
+    // Проп, який передається формі для виклику під час сабміту
+    this.props.onSubmit({ ...this.state });
+  };
+
+  render() {
+    const { name } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            placeholder="Enter name"
+            value={name}
+            onChange={this.handleChange}
+            id={this.idElenent}
+          />
+        </label>
+
+        <button type="submit">Sign up as {name}</button>
+      </form>
+    );
+  }
+}
+
+
+export default App;
