3946167a6a1bc7d438db55d4f229ba273b658e3f
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-10-18 16:32:03 +0300

    is already in contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,7 @@ class App extends Component {
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-5', name: 'Олександр', number: '4889008883' },
     ],
     filter: '',
   }
@@ -21,6 +22,16 @@ class App extends Component {
       id: nanoid(),
       name,
       number,
+    }
+
+    const filter = this.state.contacts.filter(
+      ({ name }) => name.toLowerCase() === newContact.name.toLowerCase()
+    );
+
+    if (filter.length) {
+      const { name } = newContact
+      alert(`${name} is already in contacts`)
+      return;
     }
 
     this.setState(({ contacts }) => ({
